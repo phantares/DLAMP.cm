635bf6f6cb8eef155e8b5413a506b1570b8b0818
pei <phantares11@gmail.com>
2026-03-26 04:32:17 +0800

    fix(stats): calculate target stats in original resolution

--- scripts/calculate_stats.py
+++ scripts/calculate_stats.py
@@ -8,32 +8,31 @@ import torch
 from torchvision.transforms.v2 import Resize
 
 
-variables_constant = ["terrain", "latitude", "longitude"]
+variables_static = ["terrain", "latitude", "longitude"]
 variables_single = ["sst", "psfc", "t2m", "q2m", "u10", "v10", "olr", "sw"]
-variables_upper = ["z", "u", "v", "w", "t", "qv", "qi", "qs", "qg", "qc", "qr"]
-variables_cloud = ["qi", "qs", "qg", "qc", "qr"]
+variables_upper = ["z", "u", "v", "w", "t", "qv"]
+variables_target = ["qi", "qs", "qg", "qc", "qr"]
 
 
-def main(resolution, shape, input_dir=None, output_dir=None, cloud_threshold=1e-6):
+def main(resolution, shape, input_dir=None, output_dir=None, cloud_threshold=1e-8):
     env = dotenv_values(".env")
 
     dir = Path(input_dir or env.get("INPUT_DIR"))
-    files = sorted(dir.glob("*.h5"))
+    files = [h5.File(file, "r") for file in sorted(dir.glob("*.h5"))]
     results = {}
 
-    for variable in variables_constant:
+    for variable in variables_static:
         print(variable)
 
-        with h5.File(files[0], "r") as f:
-            var = np.array(f[variable][:])
-            var[var < 0] = 0
-            var = _to_3d(var)
+        var = np.array(files[0][variable][:])
+        var[var < 0] = 0
+        var = _to_3d(var)
 
-            if variable in ["longitude", "latitude"]:
-                resize = Resize(shape, antialias=False)
-            else:
-                resize = Resize(shape)
-            var = resize(var)
+        if variable in ["longitude", "latitude"]:
+            resize = Resize(shape, antialias=False)
+        else:
+            resize = Resize(shape)
+        var = resize(var)
 
         results[variable] = {
             "pipeline": [
@@ -56,14 +55,13 @@ def main(resolution, shape, input_dir=None, output_dir=None, cloud_threshold=1e-
 
         vars = []
         for file in files:
-            with h5.File(file, "r") as f:
-                var = np.array(f[variable][:])
-                if variable not in ["u10", "v10"]:
-                    var[var < 0] = 0
-                var = _to_3d(var)
-                var = Resize(shape)(var)
+            var = np.array(file[variable][:])
+            if variable not in ["u10", "v10"]:
+                var[var < 0] = 0
+            var = _to_3d(var)
+            var = Resize(shape)(var)
 
-                vars.append(var)
+            vars.append(var)
 
         vars = torch.concatenate(vars, axis=0)
 
@@ -83,8 +81,7 @@ def main(resolution, shape, input_dir=None, output_dir=None, cloud_threshold=1e-
             ],
         }
 
-    with h5.File(files[0], "r") as f:
-        pressure = f["pressure"][:]
+    pressure = files[0]["pressure"][:]
 
     for variable in variables_upper:
 
@@ -93,21 +90,18 @@ def main(resolution, shape, input_dir=None, output_dir=None, cloud_threshold=1e-
             vars = []
 
             for file in files:
-                with h5.File(file, "r") as f:
-                    var = np.array(
-                        f[variable][
-                            :,
-                            k,
-                        ]
-                    )
-                    if variable not in ["u", "v", "w"]:
-                        var[var < 0] = 0
-                    if variable in variables_cloud:
-                        var[var < cloud_threshold] = 0
-                    var = _to_3d(var)
-                    var = Resize(shape)(var)
+                var = np.array(
+                    file[variable][
+                        :,
+                        k,
+                    ]
+                )
+                if variable not in ["u", "v", "w"]:
+                    var[var < 0] = 0
+                var = _to_3d(var)
+                var = Resize(shape)(var)
 
-                    vars.append(var)
+                vars.append(var)
 
             vars = torch.concatenate(vars, axis=0)
 
@@ -127,52 +121,89 @@ def main(resolution, shape, input_dir=None, output_dir=None, cloud_threshold=1e-
                 ],
             }
 
-            if variable in variables_cloud:
-                if torch.any(vars > 0):
-                    pr10 = np.percentile(vars[vars > 0].cpu().numpy(), 10).item()
-                    log1 = torch.log10(vars / pr10 + 1)
-                    pr10_1 = np.percentile(log1[log1 > 0].cpu().numpy(), 10).item()
-                    log2 = torch.log10(log1 / pr10_1 + 1)
+            results[f"{variable}{int(p)}"] = stats
 
-                    stats = {
-                        "pipeline": [
-                            {
-                                "type": "log",
-                                "params": {"ref": float(pr10)},
-                                "metadata": {
-                                    "mean": float(torch.mean(vars)),
-                                    "std": float(torch.std(vars)),
-                                    "min": float(torch.min(vars)),
-                                    "max": float(torch.max(vars)),
-                                },
+    for variable in variables_target:
+
+        for k, p in enumerate(pressure):
+            print(f"{variable}{int(p)}")
+            vars = []
+
+            for file in files:
+                var = np.array(
+                    file[variable][
+                        :,
+                        k,
+                    ]
+                )
+                var[var < cloud_threshold] = 0
+                var = _to_3d(var)
+
+                vars.append(var)
+
+            vars = torch.concatenate(vars, axis=0)
+
+            stats = {
+                "pipeline": [
+                    {
+                        "type": "minmax",
+                        "params": {
+                            "min": float(torch.min(vars)),
+                            "max": float(torch.max(vars)),
+                        },
+                        "metadata": {
+                            "mean": float(torch.mean(vars)),
+                            "std": float(torch.std(vars)),
+                        },
+                    },
+                ],
+            }
+
+            if torch.any(vars > 0):
+                pr10 = np.percentile(vars[vars > 0].cpu().numpy(), 10).item()
+                log1 = torch.log10(vars / pr10 + 1)
+                pr10_1 = np.percentile(log1[log1 > 0].cpu().numpy(), 10).item()
+                log2 = torch.log10(log1 / pr10_1 + 1)
+
+                stats = {
+                    "pipeline": [
+                        {
+                            "type": "log",
+                            "params": {"ref": float(pr10)},
+                            "metadata": {
+                                "mean": float(torch.mean(vars)),
+                                "std": float(torch.std(vars)),
+                                "min": float(torch.min(vars)),
+                                "max": float(torch.max(vars)),
                             },
-                            {
-                                "type": "log",
-                                "params": {"ref": float(pr10_1)},
-                                "metadata": {
-                                    "mean": float(torch.mean(log1)),
-                                    "std": float(torch.std(log1)),
-                                    "min": float(torch.min(log1)),
-                                    "max": float(torch.max(log1)),
-                                },
+                        },
+                        {
+                            "type": "log",
+                            "params": {"ref": float(pr10_1)},
+                            "metadata": {
+                                "mean": float(torch.mean(log1)),
+                                "std": float(torch.std(log1)),
+                                "min": float(torch.min(log1)),
+                                "max": float(torch.max(log1)),
                             },
-                            {
-                                "type": "minmax",
-                                "params": {
-                                    "min": float(torch.min(log2)),
-                                    "max": float(torch.max(log2)),
-                                },
-                                "metadata": {
-                                    "mean": float(torch.mean(log2)),
-                                    "std": float(torch.std(log2)),
-                                },
+                        },
+                        {
+                            "type": "minmax",
+                            "params": {
+                                "min": float(torch.min(log2)),
+                                "max": float(torch.max(log2)),
                             },
-                        ],
-                    }
+                            "metadata": {
+                                "mean": float(torch.mean(log2)),
+                                "std": float(torch.std(log2)),
+                            },
+                        },
+                    ],
+                }
 
             results[f"{variable}{int(p)}"] = stats
 
-    output_file = Path(output_dir or env.get("STATS_DIR"), f"stats_{resolution}km.json")
+    output_file = Path(output_dir or env.get("STATS_DIR"), f"qc_{resolution}km.json")
     with open(output_file, "w") as f:
         json.dump(results, f, indent=2)
 
@@ -195,7 +226,7 @@ if __name__ == "__main__":
     )
     parser.add_argument("--input_dir", default=None)
     parser.add_argument("--output_dir", default=None)
-    parser.add_argument("--cloud_threshold", default=1e-6)
+    parser.add_argument("--cloud_threshold", default=1e-8, type=float)
     args = parser.parse_args()
 
     main(
