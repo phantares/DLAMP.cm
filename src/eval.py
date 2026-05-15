fix(eval): eval per file and change precision to run on gpu

src/eval.py
@@ -1,3 +1,7 @@
+import os
+
+os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "expandable_segments:True"
+
 import argparse
 from pathlib import Path
 from dotenv import dotenv_values
@@ -14,14 +18,14 @@ from dataset import DataIndexer
 from utils import find_best_model, get_scaler_map, write_file
 
 
-def main(exp_name, wandb_id=None):
+def main(exp_name):
     env = dotenv_values(".env")
 
     with initialize(config_path=f"../experiments/{exp_name}/.hydra", version_base=None):
         cfg = compose(config_name="config")
 
     dtype = getattr(torch, cfg.dtype, torch.float32)
-    torch.set_default_dtype(dtype)
+    use_mask = cfg.model.system.get("use_mask", False)
 
     experiment_name = cfg.experiment.name
     print(f"Evaluating experiment: {experiment_name}")
@@ -30,52 +34,25 @@ def main(exp_name, wandb_id=None):
     cfg.dataset.res.column_km = column_km
     cfg.dataset.res.crop_number = 1
 
-    datamodule = hydra.utils.instantiate(
-        cfg.dataset, input_dir=Path(env.get("INPUT_DIR")), dtype=dtype
-    )
-
     checkpoint_path = find_best_model(exp_name)
     model_class = hydra.utils.get_class(cfg.model.system._target_)
 
     model = model_class.load_from_checkpoint(checkpoint_path, column_km=column_km)
-    model.to(dtype)
+    model.to(torch.float16 if use_mask else dtype)
 
-    logger = hydra.utils.instantiate(
-        cfg.logger,
-        **({"id": wandb_id, "resume": "allow"} if wandb_id else {"mode": "disabled"}),
-    )
+    logger = hydra.utils.instantiate(cfg.logger, mode="disabled")
 
     trainer = Trainer(
         logger=logger,
         accelerator="auto",
         devices=1,
+        precision="16-true" if use_mask else "16-mixed",
     )
-
-    trainer.test(model, datamodule)
-
-    test_outputs = {k: torch.cat(v).to("cpu") for k, v in model.test_outputs.items()}
 
     scaler_map = get_scaler_map(
         cfg.dataset.res.stats_file,
         **{var: cfg.dataset.var.z_target for var in cfg.dataset.var.target},
     )
-
-    for c, variable in enumerate(cfg.dataset.var.target):
-        invt_pred = (
-            scaler_map[variable]
-            .inverse_transform(test_outputs["regress"][:, :, c, ...])
-            .clamp(min=0)
-        )
-
-        test_outputs["regress"][:, :, c, ...] = invt_pred
-
-    test_outputs = {
-        k: v.cpu().numpy().reshape(-1, *v.shape[2:]) for k, v in test_outputs.items()
-    }
-
-    test_indexes = DataIndexer(
-        Path(env.get("INPUT_DIR")), **cfg.dataset.split
-    ).test_index
 
     global_grid_hr = int(
         cfg.dataset.res.global_grid
@@ -84,10 +61,41 @@ def main(exp_name, wandb_id=None):
     )
     transform_grid = CenterCrop(global_grid_hr)
 
-    current_index = 0
+    test_indexes = DataIndexer(
+        Path(env.get("INPUT_DIR")), **cfg.dataset.split
+    ).test_index
+
     for input_file, group in groupby(test_indexes, key=lambda x: x["file"]):
         samples = list(group)
-        n_samples = len(samples)
+
+        datamodule = hydra.utils.instantiate(
+            cfg.dataset, input_dir=Path(env.get("INPUT_DIR")), dtype=dtype
+        )
+        datamodule.setup("test")
+
+        datamodule.test_dataset.indexes = samples
+        datamodule.setup = lambda stage: None
+
+        model.test_outputs = {k: [] for k in model.test_outputs.keys()}
+        trainer.test(model, datamodule=datamodule)
+
+        test_outputs = {
+            k: torch.cat(v).to("cpu") for k, v in model.test_outputs.items()
+        }
+
+        for c, variable in enumerate(cfg.dataset.var.target):
+            invt_pred = (
+                scaler_map[variable]
+                .inverse_transform(test_outputs["regress"][:, :, c, ...])
+                .clamp(min=0)
+            )
+
+            test_outputs["regress"][:, :, c, ...] = invt_pred
+
+        test_outputs = {
+            k: v.cpu().numpy().reshape(-1, *v.shape[2:])
+            for k, v in test_outputs.items()
+        }
 
         indices = [s["index"] for s in samples]
         with h5.File(input_file, "r") as f:
@@ -133,15 +141,10 @@ def main(exp_name, wandb_id=None):
             input_file,
             output_file,
             new_coords,
-            {
-                k: v[current_index : current_index + n_samples]
-                for k, v in test_outputs.items()
-            },
+            test_outputs,
             test_targets,
             cfg.dataset.var.target,
         )
-
-        current_index += n_samples
 
 
 if __name__ == "__main__":
@@ -152,11 +155,6 @@ if __name__ == "__main__":
         type=str,
         help="Enter experiment name.",
     )
-    parser.add_argument(
-        "--id",
-        type=str,
-        help="Enter wandb id.",
-    )
     args = parser.parse_args()
 
-    main(args.exp, args.id)
+    main(args.exp)

src/model/systems/standard_downscaling.py
@@ -150,7 +150,15 @@ class StandardDownscaling(L.LightningModule):
         return output
 
     def general_step(
-        self, single, upper, time, target, column_bottom, column_left, shuffle=False
+        self,
+        single,
+        upper,
+        time,
+        target,
+        column_bottom,
+        column_left,
+        shuffle=False,
+        compute_loss_var=True,
     ):
         column_km = (
             torch.tensor([target["regress"].shape[-1] * self.hparams.resolution_target])
@@ -198,9 +206,12 @@ class StandardDownscaling(L.LightningModule):
             loss["total"] = nn.MSELoss()(output["regress"], target["regress"])
             output_result = output["regress"]
 
-        loss_var = nn.MSELoss(reduction="none")(output_result, target["regress"]).mean(
-            dim=(0, 1, -1, -2)
-        )
+        if compute_loss_var:
+            loss_var = nn.MSELoss(reduction="none")(
+                output_result, target["regress"]
+            ).mean(dim=(0, 1, -1, -2))
+        else:
+            loss_var = None
 
         return loss, loss_var, output
 
@@ -248,23 +259,35 @@ class StandardDownscaling(L.LightningModule):
         if self.hparams.use_mask:
             target["mask"] = (target_regress > 0).float()
 
-        loss, loss_var, output = self.general_step(
-            single, upper, time, target, column_bottom, column_left
+        loss, _, output = self.general_step(
+            single,
+            upper,
+            time,
+            target,
+            column_bottom,
+            column_left,
+            compute_loss_var=False,
+        )
+
+        loss = {k: v.detach() for k, v in loss.items()}
+        self.log(
+            f"total_test",
+            loss["total"].item(),
+            on_step=False,
+            on_epoch=True,
+            prog_bar=True,
+            sync_dist=True,
         )
 
         output = {k: v.detach().cpu() for k, v in output.items()}
         [self.test_outputs[k].append(v) for k, v in output.items()]
-
-        self._log_loss_var(
-            loss, loss_var, self.hparams.target_var, self.hparams.z_target, "test"
-        )
 
     def _log_loss_var(self, loss, loss_var, variable_name, level, stage):
         if self.hparams.use_mask:
             self.log(
                 f"mask_{stage}",
                 loss["mask"],
-                on_step=stage != "test",
+                on_step=True,
                 on_epoch=True,
                 prog_bar=True,
                 sync_dist=True,
@@ -272,7 +295,7 @@ class StandardDownscaling(L.LightningModule):
             self.log(
                 f"regress_{stage}",
                 loss["regress"],
-                on_step=stage != "test",
+                on_step=True,
                 on_epoch=True,
                 prog_bar=True,
                 sync_dist=True,
@@ -281,7 +304,7 @@ class StandardDownscaling(L.LightningModule):
         self.log(
             f"total_{stage}",
             loss["total"],
-            on_step=stage != "test",
+            on_step=True,
             on_epoch=True,
             prog_bar=True,
             sync_dist=True,