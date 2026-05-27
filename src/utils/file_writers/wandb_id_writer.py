3d026b0fe0f06bfa0cd9820a9534245c256b94ce
phantares <112887894+phantares@users.noreply.github.com>
2026-05-27 15:32:31 +0800

    fix: get correct wandb id when using multi GPU and offline mode (#52)

--- src/utils/file_writers/wandb_id_writer.py
+++ src/utils/file_writers/wandb_id_writer.py
@@ -3,6 +3,8 @@ import json
 
 
 def write_wandb_id(run_id: str, id_file: Path) -> None:
+    id_file.parent.mkdir(parents=True, exist_ok=True)
+
     with open(id_file, "w") as f:
         json.dump({"run_id": run_id}, f)
 
