8b1a2174bc1a5dce48792589000d9fe970aa910b
pei <phantares11@gmail.com>
2026-09-23 12:46:05 +0800

    fix: get data depending on target time and initial time when predicting

--- src/predict.py
+++ src/predict.py
@@ -12,7 +12,14 @@ from torchvision.transforms.v2 import CenterCrop, Compose, Resize
 from utils import write_h5_file
 
 
-def main(exp_name, target_time, source_name, input_dir, batch_size=2):
+def main(
+    exp_name,
+    input_dir,
+    source_name: str,
+    target_time: datetime | None = None,
+    initial_time: datetime | None = None,
+    batch_size: int = 1,
+):
     env = dotenv_values(".env")
 
     with initialize(config_path=f"../experiments/{exp_name}/.hydra", version_base=None):
@@ -28,12 +35,20 @@ def main(exp_name, target_time, source_name, input_dir, batch_size=2):
     transform_input = Compose([CenterCrop(grid_high), Resize(grid_low)])
     transform_high = CenterCrop(grid_high)
 
-    example_file = Path(env.get("INPUT_DIR"), f"{target_time.strftime('%Y%m')}.h5")
-    input_file = (
-        example_file
-        if source_name == "RWRF"
-        else input_dir / f"{target_time.strftime('%Y%m%d_%H%M')}.h5"
-    )
+    if source_name != "RWRF":
+        input_dir = input_dir / source_name
+
+    if initial_time is not None:
+        input_files = [input_dir / f"{initial_time:%Y%m%d_%H%M}.h5"]
+
+    elif target_time is None:
+        input_files = sorted(input_dir.glob("*.h5"))
+
+    else:
+        input_files = [input_dir / f"{target_time:%Y%m}.h5"]
+
+    output_dir = Path(env.get("OUTPUT_DIR"), exp_name, source_name)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     sess_options = ort.SessionOptions()
     sess_options.intra_op_num_threads = 12
@@ -47,169 +62,178 @@ def main(exp_name, target_time, source_name, input_dir, batch_size=2):
         providers=["CUDAExecutionProvider", "CPUExecutionProvider"],
     )
 
-    inputs = {}
-    with h5.File(input_file, "r") as f:
-        datas_static = []
-        for var in cfg.dataset.var.input_static:
-            data = torch.from_numpy(f[var][:])
-            if var not in ["longitude", "latitude"]:
-                data = data.clamp(min=0.0)
+    for input_file in input_files:
+        inputs = {}
+        with h5.File(input_file, "r") as f:
+            datas_static = []
+            for var in cfg.dataset.var.input_static:
+                data = torch.from_numpy(f[var][:])
+                if var not in ["longitude", "latitude"]:
+                    data = data.clamp(min=0.0)
 
-            if source_name == "RWRF":
-                transform = (
-                    transform_grid
-                    if var in ["longitude", "latitude"]
-                    else transform_input
+                if source_name == "RWRF":
+                    transform = (
+                        transform_grid
+                        if var in ["longitude", "latitude"]
+                        else transform_input
+                    )
+                    data = transform(data.unsqueeze(0).to(dtype))
+
+                datas_static.append(data)
+
+            datas_static = torch.stack(datas_static, dim=1)
+
+            time = [datetime.fromisoformat(t.decode("utf-8")) for t in f["time"]]
+            if target_time is not None:
+                target_index = next(
+                    (i for i, t in enumerate(time) if t == target_time), None
                 )
-                data = transform(data.unsqueeze(0).to(dtype))
+                print(time[target_index])
+                target_index = slice(target_index, target_index + 1)
+            else:
+                target_index = slice(None)
+            time = time[target_index]
+            n = len(time)
 
-            datas_static.append(data)
+            datas_single = []
+            for var in cfg.dataset.var.input_single:
+                data = torch.from_numpy(f[var][target_index,])
+                if var not in ["u10", "v10"]:
+                    data = data.clamp(min=0.0)
+                if source_name == "RWRF":
+                    data = transform_input(data.to(dtype))
+                datas_single.append(data)
 
-        datas_static = torch.stack(datas_static, dim=1)
-
-        time = [datetime.fromisoformat(t.decode("utf-8")) for t in f["time"]]
-        if source_name == "RWRF":
-            target_index = next(
-                (i for i, t in enumerate(time) if t == target_time), None
+            datas_single = torch.stack(datas_single, dim=1)
+            inputs["single"] = (
+                torch.cat(
+                    (datas_single, datas_static.expand(n, -1, -1, -1)),
+                    axis=1,
+                )
+                .to(dtype)
+                .numpy()
             )
-            print(time[target_index])
-            target_index = slice(target_index, target_index + 1)
-        else:
-            target_index = slice(None)
-        time = time[target_index]
-        n = len(time)
 
-        datas_single = []
-        for var in cfg.dataset.var.input_single:
-            data = torch.from_numpy(f[var][target_index,])
-            if var not in ["u10", "v10"]:
-                data = data.clamp(min=0.0)
-            if source_name == "RWRF":
-                data = transform_input(data.to(dtype))
-            datas_single.append(data)
+            pressure_in = f["pressure"][:]
+            sort_p = np.argsort(pressure_in)
+            z_up = np.searchsorted(pressure_in[sort_p], cfg.dataset.var.z_input)
+            z_up = sort_p[z_up]
 
-        datas_single = torch.stack(datas_single, dim=1)
-        inputs["single"] = (
-            torch.cat(
-                (datas_single, datas_static.expand(n, -1, -1, -1)),
-                axis=1,
-            )
+            datas_upper = []
+            for var in cfg.dataset.var.input_upper:
+                data = []
+                for z in z_up:
+                    data.append(
+                        torch.from_numpy(
+                            f[var][
+                                target_index,
+                                z,
+                            ]
+                        )
+                    )
+                data = torch.stack(data, axis=-3)
+
+                if var not in ["u", "v", "w"]:
+                    data = data.clamp(min=0.0)
+                if source_name == "RWRF":
+                    data = transform_input(data.to(dtype))
+                datas_upper.append(data)
+
+            inputs["upper"] = torch.stack(datas_upper, dim=1).to(dtype).numpy()
+
+        inputs["column_bottom"] = torch.zeros((n, 1)).to(dtype).numpy()
+        inputs["column_left"] = torch.zeros((n, 1)).to(dtype).numpy()
+        column_km = (
+            torch.tensor([grid_low * cfg.dataset.res.resolution_input])
             .to(dtype)
             .numpy()
         )
 
-        pressure_in = f["pressure"][:]
-        sort_p = np.argsort(pressure_in)
-        z_up = np.searchsorted(pressure_in[sort_p], cfg.dataset.var.z_input)
-        z_up = sort_p[z_up]
+        outputs_info = session.get_outputs()
+        outputs_chunks = {o.name: [] for o in outputs_info}
 
-        datas_upper = []
-        for var in cfg.dataset.var.input_upper:
-            data = []
-            for z in z_up:
-                data.append(
-                    torch.from_numpy(
-                        f[var][
-                            target_index,
-                            z,
-                        ]
-                    )
-                )
-            data = torch.stack(data, axis=-3)
+        for start in range(0, n, batch_size):
+            end = min(start + batch_size, n)
 
-            if var not in ["u", "v", "w"]:
-                data = data.clamp(min=0.0)
-            if source_name == "RWRF":
-                data = transform_input(data.to(dtype))
-            datas_upper.append(data)
+            batch_inputs = {k: inputs[k][start:end] for k in inputs if k != "column_km"}
+            batch_inputs["column_km"] = column_km
 
-        inputs["upper"] = torch.stack(datas_upper, dim=1).to(dtype).numpy()
+            batch_output = session.run(None, batch_inputs)
+            for i, o in enumerate(outputs_info):
+                outputs_chunks[o.name].append(batch_output[i])
 
-    inputs["column_bottom"] = torch.zeros((n, 1)).to(dtype).numpy()
-    inputs["column_left"] = torch.zeros((n, 1)).to(dtype).numpy()
-    column_km = (
-        torch.tensor([grid_low * cfg.dataset.res.resolution_input]).to(dtype).numpy()
-    )
+        outputs = {
+            name: np.concatenate(chunks, axis=0).reshape(-1, *chunks[0].shape[2:])
+            for name, chunks in outputs_chunks.items()
+        }
 
-    outputs_info = session.get_outputs()
-    outputs_chunks = {o.name: [] for o in outputs_info}
+        example_file = Path(env.get("INPUT_DIR"), f"{time[0]:%Y%m}.h5")
 
-    for start in range(0, n, batch_size):
-        end = min(start + batch_size, n)
-
-        batch_inputs = {k: inputs[k][start:end] for k in inputs if k != "column_km"}
-        batch_inputs["column_km"] = column_km
-
-        batch_output = session.run(None, batch_inputs)
-        for i, o in enumerate(outputs_info):
-            outputs_chunks[o.name].append(batch_output[i])
-
-    outputs = {
-        name: np.concatenate(chunks, axis=0).reshape(-1, *chunks[0].shape[2:])
-        for name, chunks in outputs_chunks.items()
-    }
-
-    with h5.File(example_file, "r") as f:
-        if source_name != "RWRF":
+        with h5.File(example_file, "r") as f:
             time_ref = [datetime.fromisoformat(t.decode("utf-8")) for t in f["time"]]
-            target_index = []
+            target_index_ref = []
             for i, t in enumerate(time_ref):
                 if t in time:
-                    target_index.append(i)
-            print(time_ref[target_index[0]])
-            print(time_ref[target_index[-1]])
+                    target_index_ref.append(i)
+            print(time_ref[target_index_ref[0]])
+            print(time_ref[target_index_ref[-1]])
 
-        lat = torch.from_numpy(f["latitude"][:])
-        lat = transform_high(lat.unsqueeze(0)).squeeze(0).numpy()
+            lat = torch.from_numpy(f["latitude"][:])
+            lat = transform_high(lat.unsqueeze(0)).squeeze(0).numpy()
 
-        lon = torch.from_numpy(f["longitude"][:])
-        lon = transform_high(lon.unsqueeze(0)).squeeze(0).numpy()
+            lon = torch.from_numpy(f["longitude"][:])
+            lon = transform_high(lon.unsqueeze(0)).squeeze(0).numpy()
 
-        pressure = f["pressure"][:]
-        z_tar = (
-            len(pressure)
-            - 1
-            - np.searchsorted(pressure[::-1], cfg.dataset.var.z_target)
-        )
+            pressure = f["pressure"][:]
+            z_tar = (
+                len(pressure)
+                - 1
+                - np.searchsorted(pressure[::-1], cfg.dataset.var.z_target)
+            )
 
-        datas_target = []
-        for var in cfg.dataset.var.target:
-            data = []
-            for z in z_tar:
-                data.append(
-                    torch.from_numpy(
-                        f[var][
-                            target_index,
-                            z,
-                        ]
+            datas_target = []
+            for var in cfg.dataset.var.target:
+                data = []
+                for z in z_tar:
+                    data.append(
+                        torch.from_numpy(
+                            f[var][
+                                target_index_ref,
+                                z,
+                            ]
+                        )
                     )
-                )
-            data = torch.stack(data, axis=-3)
+                data = torch.stack(data, axis=-3)
 
-            if var not in ["u", "v", "w"]:
-                data = data.clamp(min=0.0)
-            data = transform_high(data.to(dtype))
-            datas_target.append(data)
-        datas_target = torch.stack(datas_target, dim=1).to(dtype).numpy()
+                if var not in ["u", "v", "w"]:
+                    data = data.clamp(min=0.0)
+                data = transform_high(data.to(dtype))
+                datas_target.append(data)
+            datas_target = torch.stack(datas_target, dim=1).to(dtype).numpy()
 
-    output_path = Path(env.get("OUTPUT_DIR"), exp_name, source_name)
-    output_path.mkdir(parents=True, exist_ok=True)
-    output_file = output_path / f"{target_time.strftime('%Y%m%d_%H%M')}.h5"
+        if target_time is not None:
+            output_stem = f"{target_time:%Y%m%d_%H%M}"
 
-    write_h5_file(
-        input_file,
-        output_file,
-        {
-            "time": [t.isoformat().encode("utf-8") for t in time],
-            "pressure": pressure,
-            "latitude": lat,
-            "longitude": lon,
-        },
-        outputs,
-        datas_target,
-        cfg.dataset.var.target,
-        cfg.model.system.get("output_mode", "regress"),
-    )
+            if initial_time is not None:
+                output_stem += f"_I{initial_time:%Y%m%d_%H%M}"
+        else:
+            output_stem = input_file.stem
+        output_file = output_dir / f"{output_stem}.h5"
+
+        write_h5_file(
+            input_file,
+            output_file,
+            {
+                "time": [t.isoformat().encode("utf-8") for t in time],
+                "pressure": pressure,
+                "latitude": lat,
+                "longitude": lon,
+            },
+            outputs,
+            datas_target,
+            cfg.dataset.var.target,
+            cfg.model.system.get("output_mode", "regress"),
+        )
 
 
 if __name__ == "__main__":
@@ -221,9 +245,11 @@ if __name__ == "__main__":
         help="Enter experiment name.",
     )
     parser.add_argument(
-        "time",
+        "--input_dir",
+        "-id",
         type=str,
-        help="Enter target time in format YYYYmmddHH.",
+        default=dotenv_values(".env").get("INPUT_DIR"),
+        help="Enter input dir path.",
     )
     parser.add_argument(
         "--source",
@@ -233,25 +259,41 @@ if __name__ == "__main__":
         help="Enter input source name.",
     )
     parser.add_argument(
-        "--input_dir",
-        "-i",
+        "-t",
+        "--target_time",
         type=str,
-        default=dotenv_values(".env").get("INPUT_DIR"),
-        help="Enter input dir path.",
+        help="Enter target time in format YYYYmmddHH.",
+    )
+    parser.add_argument(
+        "-i",
+        "--initial_time",
+        type=str,
+        help="Enter initial time in format YYYYmmddHH.",
     )
     parser.add_argument(
         "--batch_size",
         "-b",
         type=int,
-        default=2,
+        default=1,
         help="Enter batch size.",
     )
     args = parser.parse_args()
 
+    target_time = (
+        datetime.strptime(args.target_time, "%Y%m%d%H").replace(tzinfo=timezone.utc)
+        if args.target_time
+        else None
+    )
+    initial_time = (
+        datetime.strptime(args.initial_time, "%Y%m%d%H").replace(tzinfo=timezone.utc)
+        if args.initial_time
+        else None
+    )
     main(
         args.exp,
-        datetime.strptime(args.time, "%Y%m%d%H").replace(tzinfo=timezone.utc),
-        args.source,
         Path(args.input_dir),
+        args.source,
+        target_time,
+        initial_time,
         args.batch_size,
     )
